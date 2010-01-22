9c75f78fccc3d68a3801610f2e779a27a0b670bd
gdula.pawel <gdula.pawel@4b9c69c6-9680-11de-a1ee-bbbdd2d7798b>
2010-01-22 12:00:00 +0000

    Create branch from current version (0.2)

---
+++ branches/0.2/BurningImageGrailsPlugin.groovy
@@ -0,0 +1,36 @@
+class BurningImageGrailsPlugin {
+    // the plugin version
+    def version = "0.3"
+    // the version or versions of Grails the plugin is designed for
+    def grailsVersion = "1.1 > *"
+    // the other plugins this plugin depends on
+    def dependsOn = [:]
+    // resources that are excluded from plugin packaging
+    def pluginExcludes = [
+        "grails-app/views/error.gsp"
+    ]
+
+    def author = "Pawel Gdula"
+    def authorEmail = "pawel.gdula@burningice.pl"
+    def title = "Burning Image"
+    def description = "Image manipulation plugin"
+    def documentation = "http://code.google.com/p/burningimage/wiki/Documentation"
+
+    def doWithSpring = {
+    }
+
+    def doWithApplicationContext = { applicationContext ->
+    }
+
+    def doWithWebDescriptor = { xml ->
+    }
+
+    def doWithDynamicMethods = { ctx ->
+    }
+
+    def onChange = { event ->
+    }
+
+    def onConfigChange = { event ->
+    }
+}
